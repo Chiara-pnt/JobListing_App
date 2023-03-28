71aa70d8be363f955c194c11d6e33955fc89fa60
Chiara Panato <chiarapanato22@gmail.com>
2023-03-28 10:49:08 +0200

    commit 1

---
+++ app/index.js
@@ -0,0 +1,50 @@
+//React import
+import { useState } from "react";
+
+// React Native components import
+import { View, ScrollView, SafeAreaView } from "react-native";
+
+// Native routing elements
+import { Stack, useRouter } from "expo-router";
+
+// Features imports
+import { COLORS, icons, images, SIZES } from "../constants";
+
+// Import Components
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+
+const Home = () => {
+  const router = useRouter();
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: { backgroundColor: COLORS.lightWhite },
+          headerShadowVisible: "false",
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+          ),
+          headerTitle: "",
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
---
+++ app/job-details/[id].js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  ActivityIndicator,
+  RefreshControl,
+} from "react-native";
+
+const JobDetails = () => {
+  return <Text>JobDetails</Text>;
+};
+
+export default JobDetails;
